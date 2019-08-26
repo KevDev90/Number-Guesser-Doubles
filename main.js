bc9f12ec308644864f61dcb39055d74aee9b420e
Dustin Mikusko <dustinjm713@yahoo.com>
2019-08-26 08:54:53 -0600

    Add reset button funtionality

--- main.js
+++ main.js
@@ -12,19 +12,29 @@ var challenger1GuessInput = document.querySelector('#number-guess-box1')
 var challenger2GuessInput = document.querySelector('#number-guess-box2')
 var challenger1Guess = document.getElementById('challenger1-guess-result')
 var challenger2Guess = document.getElementById('challenger2-guess-result')
+var minRange = parseInt('minRangeInput.value');
+var maxRange = parseInt('maxRangeInput.value');
+var randomNumber = function getRandomNumber(min, max) {
+  Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 
 updateButton.addEventListener('click', function() {
+  getRandomNumber(minRange, maxRange);
   minRangeChoice.innerText = `${minRangeInput.value}`;
   maxRangeChoice.innerText = `${maxRangeInput.value}`;
   minRangeInput.value = '';
   maxRangeInput.value = '';
+  console.log(randomNumber);
 });
 
 submitGuessButton.addEventListener('click', submitGuess);
 
 function submitGuess() {
   for (var i = 0; i < challenger1Name.length; i++) {
-    challenger1Name[i].innerText = `${challenger1NameInput.value} `}
+    challenger1Name[i].innerText = `${challenger1NameInput.value} `};
+  for (var i = 0; i < challenger2Name.length; i++) {
+    challenger2Name[i].innerText = `${challenger2NameInput.value} `};
   challenger1Guess.innerText = ` ${challenger1GuessInput.value}`;
   challenger2Guess.innerText = ` ${challenger2GuessInput.value}`;
 };
